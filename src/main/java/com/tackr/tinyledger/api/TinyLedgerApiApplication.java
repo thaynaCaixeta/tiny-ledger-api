f6c554cc06b93278d7253727186dea114e323688
Thayna Caixeta Rocha <thayna.caixeta@gmail.com>
2025-05-11 19:27:30 +0100

    feature/project-initialize Bootstrap project via Spring Initializr with core dependencies

---
+++ src/main/java/com/tackr/tinyledger/api/TinyLedgerApiApplication.java
@@ -0,0 +1,13 @@
+package com.tackr.tinyledger.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyLedgerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyLedgerApiApplication.class, args);
+	}
+
+}
